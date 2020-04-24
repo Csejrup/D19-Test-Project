d978c643ae24ca83a460fca17f569963f7027a4a
FilipCala <filipcala98@gmail.com>
2020-04-24 10:08:52 +0200

    Quantity of products showing at first

--- src/GUI/Controller/FXMLProductController.java
+++ src/GUI/Controller/FXMLProductController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -20,6 +21,7 @@ public class FXMLProductController extends AbstractController {
     @FXML
     private Label colaQTY, coffeeQTY, waterQTY, foodQTY, fruitsQTY, milkQTY;
     String showMilkQTY;
+
     /*----------------------------------------All FXML Button, Field, RadioButton,Label Declaration-----------------------*/
     @FXML
     void checkoutProducts(ActionEvent event) {
@@ -43,40 +45,92 @@ public class FXMLProductController extends AbstractController {
 
     }
 
-    private String get(String productName){
-        DB.selectSQL("SELECT fldQuantity From tblStorage where fldProductName = '"+productName +"'");
-         showMilkQTY = DB.getData();
-         int actQTY = Integer.parseInt(showMilkQTY)-1;
-       // DB.updateSQL("UPDATE tblStorage SET fldQuantity ="+actQTY+"WHERE fldProductName='"+productName+"'");
 
+
+    private String get(String productName) {
+        try {
+
+
+        DB.selectSQL("SELECT fldQuantity From tblStorage where fldProductName = '" + productName + "'");
+        showMilkQTY = DB.getData();
+        int actQTY = Integer.parseInt(showMilkQTY) - 1;
+        cleardata();
+        DB.updateSQL("UPDATE tblStorage SET fldQuantity ="+actQTY+"WHERE fldProductName='"+productName+"'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return showMilkQTY;
+
+    }
+    private String firsShowUP(String productName){
+        try {
+            DB.selectSQL("SELECT fldQuantity From tblStorage where fldProductName = '" + productName + "'");
+            showMilkQTY = DB.getData();
+            cleardata();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return showMilkQTY;
     }
 
-   
 
     public void MilkEvent(MouseEvent event) {
         get("Milk");
         milkQTY.setText(showMilkQTY);
     }
 
-    public void FruitEvent(MouseEvent event) { get("Fruits");
-    fruitsQTY.setText(showMilkQTY);
+    public void FruitEvent(MouseEvent event) {
+        get("Fruits");
+        fruitsQTY.setText(showMilkQTY);
     }
 
-    public void FoodEvent(MouseEvent event) { get("Burgers");
-    foodQTY.setText(showMilkQTY);
+    public void FoodEvent(MouseEvent event) {
+        get("Burgers");
+        foodQTY.setText(showMilkQTY);
     }
 
-    public void WatterEvent(MouseEvent event) { get("Water");
-    waterQTY.setText(showMilkQTY);}
+    public void WatterEvent(MouseEvent event) {
+        get("Water");
+        waterQTY.setText(showMilkQTY);
+    }
 
-    public void ColaEvent(MouseEvent event) { get("Coca Cola");
-    colaQTY.setText(showMilkQTY);}
+    public void ColaEvent(MouseEvent event) {
+        get("Coca Cola");
+        colaQTY.setText(showMilkQTY);
+    }
 
-    public void CoffeeEvent(MouseEvent event) { get("Coffee");
-    coffeeQTY.setText(showMilkQTY);}
+    public void CoffeeEvent(MouseEvent event) {
+        get("Coffee");
+        coffeeQTY.setText(showMilkQTY);
+    }
+
+
+
+    private static void cleardata() {
+        do {
+            String data = DB.getData();
+            if (data.equals(DB.NOMOREDATA)) {
+                break;
+            }
+
+        } while (true);
+    }
+
+
+    public void putLabelstog(MouseEvent event) {
+        firsShowUP("Milk");
+        milkQTY.setText(showMilkQTY);
+        firsShowUP("Water");
+        waterQTY.setText(showMilkQTY);
+        firsShowUP("Coffee");
+        coffeeQTY.setText(showMilkQTY);
+        firsShowUP("Coca Cola");
+        colaQTY.setText(showMilkQTY);
+        firsShowUP("Fruits");
+        fruitsQTY.setText(showMilkQTY);
+        firsShowUP("Burgers");
+        foodQTY.setText(showMilkQTY);
+    }
 }
-
-
 
 
